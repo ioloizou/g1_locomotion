358d472ba089e6a84487836795efbe033dc4910e
Ioannis Loizou <yiannisloizou@gmail.com>
2025-03-04 16:41:39 +0100

    Fix bug wbid mujoco

--- g1_mujoco_sim/scripts/run_simulation.py
+++ g1_mujoco_sim/scripts/run_simulation.py
@@ -41,11 +41,14 @@ class G1MujocoSimulation:
         self.model = mujoco.MjModel.from_xml_path(model_path)
         self.data = mujoco.MjData(self.model)
 
+        print("self.data.qpos ===========================\n", self.data.qpos)
+
         self.data.qpos = q_init
 
         # Real-time settings
         self.sim_timestep = self.model.opt.timestep
-        self.real_time_factor = 1.0  # 1.0 = real time
+        # print("self.sim_timestep ===========================\n", self.sim_timestep)
+        self.real_time_factor = 0.01  # 1.0 = real time
 
         # Create viewer
         self.viewer = mujoco_viewer.MujocoViewer(self.model, self.data)
@@ -62,6 +65,7 @@ class G1MujocoSimulation:
         return xbi_qpos_perm
 
     def setup_WBID(self):
+        
         # Get model from urdf
         self.xbi_model = xbi.ModelInterface2(self.urdf)
         qmin, qmax = self.xbi_model.getJointLimits()
@@ -69,10 +73,13 @@ class G1MujocoSimulation:
         # Initial Joint Configuration
         self.q = q_init.copy()
         self.dq = np.zeros(self.xbi_model.nv)
-        self.xbi_model.setJointPosition(self.q)
+        self.xbi_model.setJointPosition(self.permute_muj_to_xbi(self.q))
+        print("self.xbi_model.getJointPosition() ===========================", self.xbi_model.getJointPosition())
         self.xbi_model.setJointVelocity(self.dq)
+        print("self.xbi_model.getJointVelocity() ===========================", self.xbi_model.getJointVelocity())
         self.xbi_model.update()
         # Instantiate Variables: qddot and contact forces (3 per contact)
+        
         variables_vec = dict()
         variables_vec["qddot"] = self.xbi_model.nv
 
@@ -84,11 +91,12 @@ class G1MujocoSimulation:
         ]
 
         # Hands may be added in the future
-        contact_frames = line_foot_contact_frames
+        self.contact_frames = line_foot_contact_frames
 
-        for contact_frame in contact_frames:
+        for contact_frame in self.contact_frames:
             variables_vec[contact_frame] = 3
-            self.variables = pysot.OptvarHelper(variables_vec)
+        
+        self.variables = pysot.OptvarHelper(variables_vec)
 
         # Set CoM tracking task
         self.com = CoM(self.xbi_model, self.variables.getVariable("qddot"))
@@ -121,22 +129,22 @@ class G1MujocoSimulation:
         posture = Postural(self.xbi_model, self.variables.getVariable("qddot"))
 
         # For the base task taking only the orientation part
-        self.stack = 0.1 * self.com + 0.1 * (base % [3, 4, 5])
-        self.force_variables = list()
+        self.stack = 0.05 * self.com + 0.1 * (base % [3, 4, 5]) + 0.005 * posture
 
         for i in range(len(cartesian_contact_task_frames)):
             self.stack = self.stack + 10.*(contact_tasks[i])
 
-        for i in range(len(contact_frames)):
-            self.force_variables.append(self.variables.getVariable(contact_frames[i]))
+        self.force_variables = list()
+        for i in range(len(self.contact_frames)):
+            self.force_variables.append(self.variables.getVariable(self.contact_frames[i]))
 
         # Creates the stack.
-        self.stack = (pysot.AutoStack(self.stack) / posture) << DynamicFeasibility(
+        self.stack = (pysot.AutoStack(self.stack)) << DynamicFeasibility(
             "floating_base_dynamics",
             self.xbi_model,
             self.variables.getVariable("qddot"),
             self.force_variables,
-            contact_frames,
+            self.contact_frames,
         )
         self.stack = self.stack << JointLimits(
             self.xbi_model,
@@ -149,12 +157,12 @@ class G1MujocoSimulation:
         self.stack = self.stack << VelocityLimits(
             self.xbi_model, self.variables.getVariable("qddot"), dqmax, self.sim_timestep
         )
-        for i in range(len(contact_frames)):
-            T = self.xbi_model.getPose(contact_frames[i])
+        for i in range(len(self.contact_frames)):
+            T = self.xbi_model.getPose(self.contact_frames[i])
             mu = (T.linear, 0.8)  # rotation is world to contact
             self.stack = self.stack << FrictionCone(
-                contact_frames[i],
-                self.variables.getVariable(contact_frames[i]),
+                self.contact_frames[i],
+                self.variables.getVariable(self.contact_frames[i]),
                 self.xbi_model,
                 mu,
             )
@@ -166,17 +174,34 @@ class G1MujocoSimulation:
         self.alpha = 0.4
 
     def sim_step(self):
-
         """Perform a single simulation step."""
+        
+        # print("data.qpos ===========================")
+        # print(self.data.qpos)
+        # print("data.qvel ===========================")
+        # print(self.data.qvel)
+        
         self.xbi_model.setJointPosition(self.permute_muj_to_xbi(self.data.qpos))
-        self.xbi_model.setJointVelocity(self.dq)
+        # print('permuted qpos ===========================')
+        # print(self.permute_muj_to_xbi(self.data.qpos))
+        
+        self.xbi_model.setJointVelocity(self.data.qvel)
+        # print("self.xbi_model.getJointVelocity() ===========================")
+        # print(self.xbi_model.getJointVelocity())
+        
         self.xbi_model.update()
         
+        # print("self.com0 ===========================")
+        # print(self.com0)
 
         # Compute new reference for CoM task
-        self.com_ref[2] = self.com0[2] + self.alpha * np.sin(3.1415 * self.sim_time)
-        self.com_ref[1] = self.com0[1] + self.alpha * np.cos(3.1415 * self.sim_time)
-        self.sim_time += self.sim_timestep
+        print("self.sim_time =========================== \n", self.sim_time)
+        self.com_ref[0] = self.com0[0]
+        self.com_ref[1] = self.com0[1] 
+        # + self.alpha * np.cos(3.1415 * self.sim_time)
+        self.com_ref[2] = self.com0[2]
+        # + self.alpha * np.sin(3.1415 * self.sim_time)
+        print("self.com_ref =========================== \n", self.com0)
         self.com.setReference(self.com_ref)
 
         # Update stack
@@ -185,22 +210,55 @@ class G1MujocoSimulation:
         # Solve
         x = self.solver.solve()
         ddq = self.variables.getVariable("qddot").getValue(x)
-        
-        # Extract the force values
-        forces = []
-        for force_var in self.force_variables:
-            force = force_var.getValue(x)
-            forces.append(force)
 
-        # Update joint velocity
-        self.dq = self.dq + ddq * self.sim_timestep
 
+        # I NEED TO PASS THE CURRENT POSITION AND VEL
         self.q = self.xbi_model.sum(
             self.q, self.dq * self.sim_timestep + 0.5 * ddq * self.sim_timestep**2
         )
 
-        tau = xbi.computeInverseDynamics(self.xbi_model, self.q, self.dq, ddq, forces)
-        self.data.ctrl = tau
+        # I NDEED TO PASS THE CURRENT VEL
+        # Update joint velocity
+        self.dq = self.dq + ddq * self.sim_timestep
+
+        # Extract the force values
+        forces = list()
+        for i in range(len(self.contact_frames)):
+            force_var = self.variables.getVariable(self.contact_frames[i])
+            forces.append(force_var.getValue(x))
+        print("forces ===========================")
+        print(np.array(forces))
+        
+        # Update joint position
+        self.xbi_model.setJointAcceleration(ddq)
+        self.xbi_model.update()
+
+        tau = self.xbi_model.computeInverseDynamics()
+        # tau = np.zeros(self.xbi_model.nv)
+        print("tau ===========================")
+        
+        # Reaction Forces to torques
+        # tau = np.zeros(self.xbi_model.nv)
+        for i in range(len(self.contact_frames)):
+            Jc = self.xbi_model.getJacobian(self.contact_frames[i])
+            tau = tau - Jc[:3, :].T  @ np.array(forces[i])
+        
+        # print(tau)
+        print("hi")
+        # Exclude floating base
+        # tau[0:6] = 0.0
+        self.data.ctrl = tau[6:]
+                
+        # print("data.ctrl ===========================")
+        # print(self.data.ctrl)
+        # self.xbi_model.setJointEffort(tau)
+        
+        # print("xbi joint effort ===========================")
+        # print(self.xbi_model.getJointEffort())
+        
+        self.pass_count += 1
+        # if self.pass_count >= 100:
+            # exit()
 
         mujoco.mj_step(self.model, self.data)
 
@@ -208,6 +266,7 @@ class G1MujocoSimulation:
         """Run simple real-time simulation."""
         prev_time = time.time()
         self.sim_time = 0.0
+        self.pass_count = 0
 
         try:
             while self.running and not rospy.is_shutdown() and self.viewer.is_alive:
