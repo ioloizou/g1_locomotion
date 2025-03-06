f2968bf304317b03b30225ea3eb4491de2e5b45a
Ioannis Loizou <yiannisloizou@gmail.com>
2025-03-06 16:00:14 +0100

    Add WBID and MPC together

--- g1_mujoco_sim/scripts/run_simulation.py
+++ g1_mujoco_sim/scripts/run_simulation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+
+import sys
 import os
+# To be able to import srbd_mpc
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 import time
 import mujoco
 import mujoco_viewer
@@ -8,9 +12,11 @@ import rospkg
 import rospy
 import numpy as np
 from ttictoc import tic, toc
+import tf
 
 from config import q_init
 from wbid import WholeBodyID
+from srbd_mpc import mpc
 
 def loadURDF(pkg_path):
     with open(
@@ -40,7 +46,7 @@ class G1MujocoSimulation:
 
         # Real-time settings
         self.sim_timestep = self.model.opt.timestep
-        self.real_time_factor = 1.0  # 1.0 = real time
+        self.real_time_factor = 0.01  # 1.0 = real time
 
         # Create viewer
         self.viewer = mujoco_viewer.MujocoViewer(self.model, self.data)
@@ -59,17 +65,64 @@ class G1MujocoSimulation:
     def sim_step(self):
         """Perform a single simulation step."""
         tic()
-        
-        WBID.stepProblem(self.permute_muj_to_xbi(self.data.qpos), self.data.qvel, self.sim_time)
 
+        
+        
+        # Set the initial state for the MPC
+        q_euler = np.array(tf.transformations.euler_from_quaternion(self.permute_muj_to_xbi(self.data.qpos)[3:7]))
+        com_vel = WBID.model.getCOMJacobian() @ self.data.qvel
+        
+        MPC.x0[0:3] = q_euler.copy().reshape(3, 1)  # Get the initial position of the base
+        MPC.x0[3:6] = WBID.model.getCOM().reshape(3, 1)   # Get the initial position of the com
+        MPC.x0[6:9] = com_vel.reshape(3, 1)   # Get the initial velocity of the com
+        MPC.x0[9:12] = self.data.qvel[:3].reshape(3, 1)   # Get the initial rotation of the base
+        MPC.x0[12] = MPC.g
+        
+        # Setup reference horizon.  
+        MPC.x_ref_hor[0, :] = MPC.x0[:].copy().reshape(13)
+        MPC.x_ref_hor[0:, 3:6] = [5.26790425e-02, 7.44339342e-05, 5.97983255e-01]   
+        MPC.x_ref_hor[0:, -1] = MPC.g
+        
+        print("x0", MPC.x0) 
+        # print("x_ref_hor", MPC.x_ref_hor)
+
+        # Current foot heel and toe positions
+        left_heel = WBID.model.getPose("left_foot_line_contact_lower").translation
+        left_toe = WBID.model.getPose("left_foot_line_contact_upper").translation
+        
+        right_heel = WBID.model.getPose("right_foot_line_contact_lower").translation
+        right_toe = WBID.model.getPose("right_foot_line_contact_upper").translation
+
+        c_horizon = []
+        contact_horizon = []
+        for i in range(MPC.HORIZON_LENGTH):
+            c_horizon.append(np.concatenate((left_heel, left_toe, right_heel ,right_toe)))
+        
+        # Both feet in contact for all the horizon
+        for i in range(MPC.HORIZON_LENGTH):
+            contact_horizon.append(np.array([1, 1, 1, 1]))
+
+        p_com_horizon = MPC.x_ref_hor[:, 3:6].copy()
+
+        # Solve the MPC
+        u_opt0, x_opt1 = MPC.update(contact_horizon, c_horizon, p_com_horizon, x_current = MPC.x0 , one_rollout = True)
+
+        WBID.updateModel(self.permute_muj_to_xbi(self.data.qpos), self.data.qvel)
+        WBID.setReference(x_opt1[3:6].flatten(), u_opt0.flatten(), self.sim_time)
+        WBID.stack.update()
+        WBID.solveQP()
+
+        # WBID.stepProblem(self.permute_muj_to_xbi(self.data.qpos), self.data.qvel, self.sim_time)
+
+        
         tau = WBID.getInverseDynamics()
 
         # Exclude floating base
         self.data.ctrl = tau[6:]
 
-        # self.pass_count += 1
-        # if self.pass_count >= 1:
-        # exit()
+        self.pass_count += 1
+        if self.pass_count >= 2000:
+            exit()
 
         print("toc() ===========================", toc())
         mujoco.mj_step(self.model, self.data)
@@ -79,38 +132,45 @@ class G1MujocoSimulation:
         prev_time = time.time()
         self.sim_time = 0.0
         self.pass_count = 0
+        
+        # try:
+        while self.running and not rospy.is_shutdown() and self.viewer.is_alive:
+            # Get real time elapsed since last step
+            current_time = time.time()
+            elapsed_wall_time = current_time - prev_time
+            prev_time = current_time
 
-        try:
-            while self.running and not rospy.is_shutdown() and self.viewer.is_alive:
-                # Get real time elapsed since last step
-                current_time = time.time()
-                elapsed_wall_time = current_time - prev_time
-                prev_time = current_time
+            # Compute simulation time to advance (applying real-time factor)
+            sim_time_to_advance = elapsed_wall_time * self.real_time_factor
 
-                # Compute simulation time to advance (applying real-time factor)
-                sim_time_to_advance = elapsed_wall_time * self.real_time_factor
+            # Calculate steps needed
+            steps = max(1, int(sim_time_to_advance / self.sim_timestep))
 
-                # Calculate steps needed
-                steps = max(1, int(sim_time_to_advance / self.sim_timestep))
+            # Step the simulation
+            for _ in range(steps):
+                self.sim_step()
+                self.sim_time += self.sim_timestep
 
-                # Step the simulation
-                for _ in range(steps):
-                    self.sim_step()
-                    self.sim_time += self.sim_timestep
+            # Render the current state
+            self.viewer.render()
 
-                # Render the current state
-                self.viewer.render()
-
-        except Exception as e:
-            rospy.logerr(f"Simulation error: {e}")
-        finally:
-            if self.viewer:
-                self.viewer.close()
+        # except Exception as e:
+        #     rospy.logerr(f"Simulation error: {e}")
+        # finally:
+        #     if self.viewer:
+        #         self.viewer.close()
 
 
 if __name__ == "__main__":
+    
     # Setup the simulation
     sim = G1MujocoSimulation(q_init)
+    
+
+    # Setup the MPC
+    MPC = mpc.MPC(dt = 0.04)
+    MPC.init_matrices()
+
     # Setup the whole body ID
     WBID = WholeBodyID(sim.urdf, sim.sim_timestep, q_init)
     WBID.setupProblem()
